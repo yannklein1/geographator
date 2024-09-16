8c61a8ea10b7a4efe1f9e3673c8bded5ae5ff9b0
yannklein1 <161606294+yannklein1@users.noreply.github.com>
2024-09-16 18:28:50 +0200

    Create scriptPaysAsie.js

---
+++ scripts/scriptPaysAsie.js
@@ -0,0 +1,94 @@
+let btnValiderMot = document.getElementById("btnValiderMot")
+let btnRejouer = document.getElementById("btnRejouer")
+let listeNombresAleatoires = []
+let nombreAleatoire = Math.floor(Math.random()*(listePaysCapitalesAsie.length))
+
+function melange () {
+	nombreAleatoire = Math.floor(Math.random()*(listePaysCapitalesAsie.length))
+}
+
+function initialisation () {
+	btnValiderMot.style.display = "block";
+	btnRejouer.style.display = "none";
+	btnValiderMot.innerHTML = "Valider";
+	i = 0;
+	score = 0;
+	melange ();
+	listeNombresAleatoires.push(nombreAleatoire);
+	console.log(listeNombresAleatoires)
+	inputEcriture.disabled = false;
+	zoneQuestion.innerHTML = listePaysCapitalesAsie[nombreAleatoire].capitale
+	affichageScore.innerHTML = score + " / " + i
+}
+
+function lancerJeu () {
+	initialisation()
+	
+	//boucle
+	btnValiderMot.addEventListener("click", () => {
+		processJeu ()
+	})
+	
+	zoneSaisie.addEventListener("keyup", function(enter) {
+		if (enter.code === "Enter") {
+			processJeu ()
+		}
+	})
+}
+
+function processJeu () {
+	calculScore()
+	inputEcriture.value = ""
+	i++
+	if (i === 20) {
+        finJeu();
+    } else {
+	do {melange ()} while (listeNombresAleatoires.includes(nombreAleatoire)===true)
+	listeNombresAleatoires.push(nombreAleatoire);
+	console.log(listeNombresAleatoires);
+	affichageScore.innerHTML = score + " / " + i 
+	modificationCapitale ()
+	}
+}
+
+function modificationCapitale () {
+	zoneQuestion.innerHTML = listePaysCapitalesAsie[nombreAleatoire].capitale
+}
+
+function suppressionAccents(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function calculScore () {
+	let reponseUtilisateur = suppressionAccents(inputEcriture.value.toLowerCase().trim())
+	let bonneReponse = suppressionAccents(listePaysCapitalesAsie[nombreAleatoire].pays.toLowerCase())
+	if 	(reponseUtilisateur === bonneReponse){
+	score++;
+	affichageScore.innerHTML = score + " / " + i;
+	affichageBonneReponse ()
+	} else {
+		affichageMauvaiseReponse ()
+		}
+}
+
+function affichageBonneReponse () {
+	zoneReponsePrecedente.innerHTML = "Bonne réponse"
+}
+
+function affichageMauvaiseReponse () {
+	zoneReponsePrecedente.innerHTML = "Mauvaise réponse, la réponse était " + listePaysCapitalesAsie[nombreAleatoire].pays
+}
+
+function finJeu() {
+	affichageScore.innerHTML = score + " / " + i ;
+	inputEcriture.disabled = true;
+	zoneProposition.innerHTML = "";
+    zoneQuestion.innerHTML = "Bravo, vous avez terminé le jeu";
+	btnValiderMot.style.display = "none";
+	btnRejouer.style.display = "block";
+	btnRejouer.addEventListener("click", () => {
+		initialisation ()
+		})
+}
+
+lancerJeu ()
